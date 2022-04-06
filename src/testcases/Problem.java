538f6f1533a8947b99d1fb187113a8d74ec57f97
Semih <putnamsemih@gmail.com>
2022-04-06 19:45:52 -0400

    Created the loop in master

---
+++ src/testcases/Problem.java
@@ -0,0 +1,10 @@
+package testcases;
+
+public class Problem {
+    public static void main(String[] args) {
+        for (int j = 0; j < 5; j++) {
+            System.out.println(j);
+        }
+        //useful info
+    }
+}
